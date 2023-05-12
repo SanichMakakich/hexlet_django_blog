9c3ba433098d7119a91f37bb3d82e1c759fc9175
Александр Мышкин <sir.myshkin@gmail.com>
2023-05-13 01:18:59 +0300

    step 5

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('article')
+    item = ['Articles']
+    return render(
+        request,
+        'article/index.html',
+        context={'info': item[0]}
+    )
