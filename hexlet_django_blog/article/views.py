21b3ed3683cacab4f084dbcb7b1dee387d2c3bc7
Александр Мышкин <sir.myshkin@gmail.com>
2023-06-03 02:17:08 +0300

    step 16-17 complate

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+from django.contrib import messages
 
+from .forms import CommentArticleForm, ArticleForm
 from hexlet_django_blog.article.models import Article
 
 
@@ -24,3 +26,49 @@ class ArticleView(View):
                 'article': article
             }
         )
+
+
+class ArticleFormCreateView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'article/create.html', {'form': form, })
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():  # Если данные корректные, то сохраняем данные формы
+            form.save()
+            messages.success(request, 'Success')
+            return redirect('articles')  # Редирект на указанный маршрут
+        # Если данные некорректные, то возвращаем человека обратно на страницу с заполненной формой
+        return render(request, 'article/create.html', {'form': form})
+
+
+class ArticleFormEditView(View):
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        return render(request,
+                      'article/update.html',
+                      {'form': form,
+                       'article_id': article_id})
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles')
+        return render(request,
+                      'article/update.html',
+                      {'form': form,
+                       'article_id': article_id})
+
+
+class CommentArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = CommentArticleForm()  # Создаем экземпляр нашей формы
+        return render(request, 'comment.html', {'form': form})  # Передаем нашу форму в контексте
